fa648a5004b76b9ef2985994076f5f7b9f73fa69
Jason Botesazan <jbotesazan@gmail.com>
2021-12-14 23:55:21 -0500

    commit tree 2a6def408c83e747f0481dcc7b226000f4f71bf4

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+function EyesOnMe () {
+
+    function focus () {
+        return console.log('Good!')
+    }
+
+    function blur () {
+        return console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <button onFocus = {focus} onBlur = {blur} >Eyes on me</button>
+    )
+
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+function Keypad () {
+
+    function pWord () {
+        return console.log('Entering password...')
+    }
+
+    return (
+        <input type="password" onChange={pWord} />
+    )
+    
+}
+
+export default Keypad;
